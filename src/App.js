f4fe0f3b688f39ac6db4320317c0ab60712d21bb
AmilaWeerasinghe <amilaweerasinghe677@gmail.com>
2023-07-21 15:13:14 +0530

    added-basic-frontend-structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [newItemName, setNewItemName] = useState('');
+
+  useEffect(() => {
+    fetchItems();
+  }, []);
+
+  const fetchItems = async () => {
+    const response = await axios.get('http://localhost:5000/api/items');
+    setItems(response.data);
+  };
+
+  const handleAddItem = async () => {
+    const response = await axios.post('http://localhost:5000/api/items', {
+      name: newItemName,
+    });
+    setItems([...items, response.data]);
+    setNewItemName('');
+  };
+
+  const handleDeleteItem = async (itemId) => {
+    await axios.delete(`http://localhost:5000/api/items/${itemId}`);
+    setItems(items.filter((item) => item.id !== itemId));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Items List</h1>
+      <ul>
+        {items.map((item) => (
+          <li key={item.id}>
+            {item.name}
+            <button onClick={() => handleDeleteItem(item.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+      <div>
+        <input
+          type="text"
+          value={newItemName}
+          onChange={(e) => setNewItemName(e.target.value)}
+        />
+        <button onClick={handleAddItem}>Add Item</button>
+      </div>
     </div>
   );
 }
